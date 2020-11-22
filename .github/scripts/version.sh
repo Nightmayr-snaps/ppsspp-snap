f58bb7451fa51b95776341f8a014c2d916509286
Nightmayr <umayr23@gmail.com>
2020-11-22 15:27:34 +0000

    updating worflows

--- .github/scripts/version.sh
+++ .github/scripts/version.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
-# check latest tagged version
-LATEST_VERSION_TAG="$(curl https://api.github.com/repos/Arisotura/melonDS/releases/latest -s | jq .tag_name -r)"
-CURRENT_VERSION_SNAP="$(snap info melonds | grep edge | head -n 2 | tail -n 1 | awk -F ' ' '{print $2}')"
+
+# check latest released tagged version
+LATEST_VERSION_TAG="$(curl https://api.github.com/repos/hrydgard/ppsspp/releases/latest -s | jq .tag_name -r)"
+CURRENT_VERSION_SNAP="$(snap info ppsspp-emu | grep edge | head -n 2 | tail -n 1 | awk -F ' ' '{print $2}')"
+LATEST_VERSION=${LATEST_VERSION_TAG#v}
 
 
 # compare versions
-if [ $CURRENT_VERSION_SNAP != $LATEST_VERSION_TAG ]; then
-    echo "versions don't match, github: $LATEST_VERSION_TAG snap: $CURRENT_VERSION_SNAP"
-    echo true >> build
-    echo $CURRENT_VERSION_SNAP >> current_version
-    echo $LATEST_VERSION_TAG >> latest_version
+if [ $CURRENT_VERSION_SNAP != $LATEST_VERSION ]; then
+    echo "versions don't match, github: $LATEST_VERSION snap: $CURRENT_VERSION_SNAP"
+    echo "BUILD=true" >> $GITHUB_ENV
+    echo "LATEST_VERSION=$LATEST_VERSION" >> $GITHUB_ENV
+    echo "CURRENT_VERSION_SNAP=$CURRENT_VERSION_SNAP" >> $GITHUB_ENV
 else
-    echo "versions match, github: $LATEST_VERSION_TAG snap: $CURRENT_VERSION_SNAP"
-    echo false >> build
+    echo "versions match, github: $LATEST_VERSION snap: $CURRENT_VERSION_SNAP"
+    echo "BUILD=false" >> $GITHUB_ENV
 fi
 
 
