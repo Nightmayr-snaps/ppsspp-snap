3494bab0658eb4f19b339526035ba1bc6e5a68e7
Nightmayr <umayr23@gmail.com>
2020-11-22 16:21:41 +0000

    updating CI script

--- .github/scripts/version.sh
+++ .github/scripts/version.sh
@@ -1,21 +1,33 @@
 #!/bin/bash
 
+set -x
 
-# check latest released tagged version
-LATEST_VERSION_TAG="$(curl https://api.github.com/repos/hrydgard/ppsspp/releases/latest -s | jq .tag_name -r)"
-CURRENT_VERSION_SNAP="$(snap info ppsspp-emu | grep edge | head -n 2 | tail -n 1 | awk -F ' ' '{print $2}')"
-LATEST_VERSION=${LATEST_VERSION_TAG#v}
+snap_name="ppsspp-emu"
+git_repo="hrydgard/ppsspp"
 
-
-# compare versions
-if [ $CURRENT_VERSION_SNAP != $LATEST_VERSION ]; then
-    echo "versions don't match, github: $LATEST_VERSION snap: $CURRENT_VERSION_SNAP"
+snap info $snap_name > /dev/null
+if [ "$?" -ne 0 ]; then
+    LATEST_VERSION_TAG="$(curl https://api.github.com/repos/$git_repo/releases/latest -s | jq .tag_name -r)"
+    LATEST_VERSION=${LATEST_VERSION_TAG#v}
+    echo "Snap not found in store! Building and publishing for first time"
     echo "BUILD=true" >> $GITHUB_ENV
     echo "LATEST_VERSION=$LATEST_VERSION" >> $GITHUB_ENV
-    echo "CURRENT_VERSION_SNAP=$CURRENT_VERSION_SNAP" >> $GITHUB_ENV
 else
-    echo "versions match, github: $LATEST_VERSION snap: $CURRENT_VERSION_SNAP"
-    echo "BUILD=false" >> $GITHUB_ENV
+    # check latest released tagged version
+    LATEST_VERSION_TAG="$(curl https://api.github.com/repos/$git_repo/releases/latest -s | jq .tag_name -r)"
+    CURRENT_VERSION_SNAP="$(snap info $snap_name | grep edge | head -n 2 | tail -n 1 | awk -F ' ' '{print $2}')"
+    LATEST_VERSION=${LATEST_VERSION_TAG#v}
+
+    # compare versions
+    if [ $CURRENT_VERSION_SNAP != $LATEST_VERSION ]; then
+        echo "versions don't match, github: $LATEST_VERSION snap: $CURRENT_VERSION_SNAP"
+        echo "BUILD=true" >> $GITHUB_ENV
+        echo "LATEST_VERSION=$LATEST_VERSION" >> $GITHUB_ENV
+        echo "CURRENT_VERSION_SNAP=$CURRENT_VERSION_SNAP" >> $GITHUB_ENV
+    else
+        echo "versions match, github: $LATEST_VERSION snap: $CURRENT_VERSION_SNAP"
+        echo "BUILD=false" >> $GITHUB_ENV
+    fi
 fi
 
 
